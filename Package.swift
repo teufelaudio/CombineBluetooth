dc28d1839ebfe507f6288339c54e510eb59de098
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2020-05-24 19:11:21 +0200

    Dynamic linking option for mocks

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     products: [
         .library(name: "CombineBluetooth", targets: ["CombineBluetooth"]),
         .library(name: "CombineBluetoothDynamic", type: .dynamic, targets: ["CombineBluetooth"]),
-        .library(name: "CombineBluetoothMocks", targets: ["CombineBluetoothMocks"])
+        .library(name: "CombineBluetoothMocks", targets: ["CombineBluetoothMocks"]),
+        .library(name: "CombineBluetoothMocksDynamic", type: .dynamic, targets: ["CombineBluetoothMocks"])
     ],
     targets: [
         .target(name: "CombineBluetooth"),

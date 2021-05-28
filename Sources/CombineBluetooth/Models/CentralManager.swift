05100d1e3cf201acde0ccb8ff5df3c261210d1fb
Giulia Ariu <giulia.ariu@teufel.de>
2021-05-28 18:34:15 +0200

    Change serviceUUIDs to optional in scanForPeripherals

--- Sources/CombineBluetooth/Models/CentralManager.swift
+++ Sources/CombineBluetooth/Models/CentralManager.swift
@@ -6,8 +6,8 @@ public protocol CentralManager: BluetoothManager {
     var isScanning: AnyPublisher<Bool, Never> { get }
     var peripheralConnection: AnyPublisher<PeripheralConnectionEvent, Never> { get }
     func scanForPeripherals() -> AnyPublisher<AdvertisingPeripheral, BluetoothError>
-    func scanForPeripherals(withServices serviceUUIDs: [CBUUID]) -> AnyPublisher<AdvertisingPeripheral, BluetoothError>
-    func scanForPeripherals(withServices serviceUUIDs: [CBUUID], options: [String: Any]) -> AnyPublisher<AdvertisingPeripheral, BluetoothError>
+    func scanForPeripherals(withServices serviceUUIDs: [CBUUID]?) -> AnyPublisher<AdvertisingPeripheral, BluetoothError>
+    func scanForPeripherals(withServices serviceUUIDs: [CBUUID]?, options: [String: Any]) -> AnyPublisher<AdvertisingPeripheral, BluetoothError>
     func scanForPeripherals(options: [String: Any]) -> AnyPublisher<AdvertisingPeripheral, BluetoothError>
     func retrievePeripherals(withIdentifiers identifiers: [UUID]) -> [BluetoothPeripheral]
     func retrieveConnectedPeripherals(withServices serviceUUIDs: [CBUUID]) -> [BluetoothPeripheral]

79a8b6fe675944f6ff52d1cda4f1aa643c1573db
Luiz Rodrigo Martins Barbosa <luiz@developercity.de>
2021-09-27 11:57:52 +0200

    Support Xcode 13

--- Sources/CombineBluetooth/Internal/CoreBluetoothService.swift
+++ Sources/CombineBluetooth/Internal/CoreBluetoothService.swift
@@ -11,7 +11,11 @@ struct CoreBluetoothService: Identifiable {
 }
 
 extension CoreBluetoothService: BluetoothService {
-    var peripheral: UUID { service.peripheral.identifier }
+    #if swift(>=5.5) && !os(macOS)
+    var peripheral: UUID? { service.peripheral?.identifier }
+    #else
+    var peripheral: UUID? { service.peripheral.identifier }
+    #endif
     var isPrimary: Bool { service.isPrimary }
     var includedServices: [BluetoothService]? { service.includedServices?.map(CoreBluetoothService.init) }
     var characteristics: [BluetoothCharacteristic]? { service.characteristics?.map(CoreBluetoothCharacteristic.init) }
--- Sources/CombineBluetooth/Models/BluetoothCharacteristic.swift
+++ Sources/CombineBluetooth/Models/BluetoothCharacteristic.swift
@@ -3,7 +3,7 @@ import CoreBluetooth
 // sourcery: AutoMockable
 public protocol BluetoothCharacteristic {
     var id: CBUUID { get }
-    var service: BluetoothService { get }
+    var service: BluetoothService? { get }
     var properties: CBCharacteristicProperties { get }
     var value: Data? { get }
     var descriptors: [BluetoothDescriptor]? { get }
